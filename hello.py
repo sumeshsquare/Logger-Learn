bd327f95240a54caba7966d01578aba266b83178
sumeshsquare <sumeshsquare@gmail.com>
2018-11-25 11:20:36 +0530

    new

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h2> Welcome to Reddiars Learning APP for kids </h2>'
+	return '<h2> Welcome to Reddiars Learning APP for kids </h2>  <img src="imag.png" alt="Smiley face" > '
 
 if __name__ == "__main__":
 	app.run()
