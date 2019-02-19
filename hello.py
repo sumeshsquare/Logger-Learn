7993bd54eaaa97ce0c57d4ae0de1b74d3ba08869
sumeshsquare <sumeshsquare@gmail.com>
2019-02-19 22:37:31 +0530

    edits2

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1> <center> Welcome to CareBook !!! </center> </h1>'
+	return '<h1> <center> Welcome to CareBook !!! </center> </h1> <p></p> <h3> <center> A book for the regular checkups </center></h3>'
 
 if __name__ == "__main__":
 	app.run()
