8cef21b2ebca1d1cbc95faeae9fbd7e326d20b50
sumeshsquare <sumeshsquare@gmail.com>
2019-07-28 15:27:13 +0530

    Edit-1

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-	return '<h1> <center> Welcome to CareBook !!! </center> </h1> <p></p> <h3> <center> A book for the regular checkups </center></h3>'
+   return render_template("index.html")
 
 if __name__ == "__main__":
 	app.run()
